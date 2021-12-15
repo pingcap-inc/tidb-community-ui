eebd83179970d02bbd2e6fb4fc038c11ffe01cae
Jagger <634750802@qq.com>
2021-12-16 07:54:20 +0800

    feat: support dev

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,11 +1,39 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials'
   ],
-  "framework": "@storybook/react"
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5'
+  },
+  webpackFinal: async (config, { configType }) => {
+    if (configType === 'DEVELOPMENT') {
+      config.module.rules.push({
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true,
+              lessOptions: {
+                javascriptEnabled: true
+              }
+            }
+          }]
+      })
+    }
+    return config
+  }
 }
--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -1,4 +1,8 @@
-import '@pingcap-inc/tidb-community-ui/dist/antd.css'
+if (process.env.NODE_ENV === 'production') {
+  require('@pingcap-inc/tidb-community-ui/dist/antd.css')
+} else {
+  require('@pingcap-inc/tidb-community-ui/theme/index.less')
+}
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
