c350b56117d9ee21c0008d00d168564c7028488e
Jagger <634750802@qq.com>
2021-12-16 21:53:05 +0800

    chore: add storybook example

--- packages/storybook/stories/antd/basic/Typography.jsx
+++
@@ -1,7 +0,0 @@
-export default function Typography ({ cn, children }) {
-  return (
-    <span className={`typography ${typeof cn === 'string' ? cn : cn.join(' ')}`}>
-      {children}
-    </span>
-  )
-}
