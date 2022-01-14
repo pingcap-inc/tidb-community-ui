424d10e8a80849fc5ccc65243068e832c2dba7ff
Jagger <634750802@qq.com>
2022-01-14 12:54:38 +0800

    feat: login & logout

--- packages/site-components/src/utils/account.ts
+++ packages/site-components/src/utils/account.ts
@@ -3,18 +3,22 @@ import { getUrl, Site } from './site'
 
 export function login () {
   const { site, env } = getSiteComponentsConfig()
-  getUrl(site, env, {
+  const { url } = getUrl(site, env, {
     site: Site.accounts,
     url: `/login?redirect_to=${encodeURIComponent(location.href)}`,
     newWindow: false,
   })
+
+  location.href = url
 }
 
 export function logout () {
   const { site, env } = getSiteComponentsConfig()
-  getUrl(site, env, {
+  const { url } = getUrl(site, env, {
     site: Site.accounts,
     url: `/logout?redirect_to=${encodeURIComponent(location.href)}`,
     newWindow: false,
   })
+
+  location.href = url
 }
