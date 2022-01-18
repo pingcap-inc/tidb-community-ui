bf807c91e1b74a36678a98602eb3207790262410
Jagger <634750802@qq.com>
2022-01-19 02:31:54 +0800

    feat: asktug support read

--- packages/asktug-theme/src/fetchers.ts
+++ packages/asktug-theme/src/fetchers.ts
@@ -23,13 +23,29 @@ const accounts: Fetcher = (key: string) => {
   }
 }
 
+const tryJson = (data: string): object | string => {
+  const trimmedData = data.trim()
+  try {
+    const json = JSON.parse(trimmedData)
+    if (typeof json === 'object') {
+      return json
+    } else {
+      return data
+    }
+  } catch (e) {
+    return data
+  }
+}
+
 const asktug: Fetcher = (key: string, params) => {
   if (typeof params === 'string') {
-    params = JSON.parse(params)
+    params = tryJson(params)
   }
   switch (key) {
     case 'asktug.getNotifications':
       return fetch(`/notifications?${stringify(params)}`, { headers: { accept: 'application/json' } }).then(processResponse)
+    case 'asktug.readNotification':
+      return fetch(`/notifications/mark-read`, { headers: { accept: 'application/json' }, body: JSON.stringify({ id: params }) })
     default:
       throw new Error('not implemented')
   }
