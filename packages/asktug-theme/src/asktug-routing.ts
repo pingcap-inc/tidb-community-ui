53a4fd21e90b093bd7c8cc19c0686ac5ce13b0cf
Jagger <634750802@qq.com>
2022-01-19 20:08:30 +0800

    feat: improve header

--- packages/asktug-theme/src/asktug-routing.ts
+++ packages/asktug-theme/src/asktug-routing.ts
@@ -1,5 +1,10 @@
+declare global {
+  interface Window {
+    Discourse: any
+  }
+}
+
 export function routeTo (url: string) {
-  // TODO: find a nicer entry
-  const app = (window as any).Discourse._applicationInstances.values().next().value
-  app.router.transitionTo(url)
+  const router = window.Discourse.__container__.lookup('service:router')
+  router.transitionTo(url)
 }
