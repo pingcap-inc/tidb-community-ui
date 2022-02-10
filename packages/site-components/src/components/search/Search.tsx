7261b9f48f2a5421bc1e5306ad664ddfb9b51a6b
Jagger <634750802@qq.com>
2022-02-10 15:21:02 +0800

    feat: update search st4yle

--- packages/site-components/src/components/search/Search.tsx
+++ packages/site-components/src/components/search/Search.tsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react'
-import { Input } from 'antd'
-import { SearchOutlined } from '@ant-design/icons'
+import React, {useState} from 'react'
+import {SearchOutlined} from '@ant-design/icons'
 
-const Search = ({ style }: { style?: React.CSSProperties }): JSX.Element => {
+const Search = ({style}: { style?: React.CSSProperties }): JSX.Element => {
 
   const [q, setQ] = useState('')
 
   const href = `https://search.asktug.com/blog?q=${encodeURIComponent(q)}`
 
+  const handle = () => {
+    window.open(href, '_blank')
+  }
+
   return (
-    <Input.Group style={style} className='ti-site-search' compact>
-      <Input
-        className='ti-site-search__input'
-        placeholder='全站搜索...'
+    <div className='ti-site-search'>
+      <input
         value={q}
         onChange={e => setQ(e.target.value)}
+        placeholder='全站搜索'
+        onKeyPress={e => e.key === 'Enter' && handle()}
       />
-      <a className="ant-btn ant-btn-primary ant-btn-sm" href={href}>
+      <a href={href} target='_blank' rel='noreferrer'>
         <SearchOutlined />
       </a>
-    </Input.Group>
-
+    </div>
   )
 }
 
