122c4f2e6a52837b39b44ff1bb07d65ec1bb84e4
Chang Wei <changwei1006@gmail.com>
2022-12-01 11:33:37 +0800

    update @pingcap-inc/tidb-community-site-components: update SidebarEvent, fix style

--- packages/site-components/src/components/asktug/home/Sidebar/SidebarEvent.tsx
+++ packages/site-components/src/components/asktug/home/Sidebar/SidebarEvent.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react"
 import dayjs from "dayjs";
-import {Badge} from "antd";
+import {Badge, Divider, Space} from "antd";
+import axios from "axios";
 
 import './SidebarEvent.less'
 import IconSvg from './event-icon.svg'
@@ -26,10 +27,16 @@ export interface IEvent {
 }
 
 const getEvents = async () => {
-  //const url = 'http://localhost:3300/next-api/cms/tidbio-homepage-main-activities'
-  //const response = await axios.get<IEvent[]>(url)
-  //return response.data
-  return []
+  const url = 'http://localhost:3000/next-api/cms/tidbio-homepage-main-activities'
+  let response
+  try {
+    response = await axios.get<IEvent[]>(url)
+  } catch (e) {
+    console.error('getEvents', e)
+    return []
+  }
+  return response?.data ?? []
+  //return []
 }
 
 const SidebarEvent: React.FC<IProps> = (props) => {
@@ -43,36 +50,34 @@ const SidebarEvent: React.FC<IProps> = (props) => {
   return (
     <div className={'asktug-sidebar-event'}>
       <SidebarCard header={{start: (<>活动日历 <IconSvg /></>), end: (<SiteLink site={Site.home} newWindow url={'/event'}>更多 {'>'}</SiteLink>)}}>
-        <ul>
+        <Space direction={'vertical'} size={16} split={<Divider style={{margin: 0}} />}>
           {events.map((value) => {
             const dayObj = dayjs(value.date);
             const month = dayObj.format('MMM');
             const day = dayObj.format('D');
             return (
-              <li key={value.id}>
-                <SiteLink site={Site.home} newWindow url={`/event`}>
-                  <div className={'asktug-sidebar-event-container'}>
-                    <div className={'asktug-sidebar-event-container-calendar'}>
-                      <div className={'asktug-sidebar-event-container-calendar-head'}>{month}</div>
-                      <div className={'asktug-sidebar-event-container-calendar-body'}>
-                        {day}
-                        <CaretUpOutlined className="asktug-sidebar-event-container-calendar-body-arrow" />
-                        <CaretUpOutlined className="asktug-sidebar-event-container-calendar-body-arrow-oppsite" />
-                      </div>
-                    </div>
-                    <div className={'asktug-sidebar-event-container-content'}>
-                      <h3>{value.title}</h3>
-                      <div>
-                        {/*{value.status && <Badge color={'#ee6d85'} text={value.status} />}*/}
-                        {value.type && <Badge color={'#69c384'} text={value.type} />}
-                      </div>
+              <SiteLink key={value.id} site={Site.home} newWindow url={`/event`}>
+                <div className={'asktug-sidebar-event-container'}>
+                  <div className={'asktug-sidebar-event-container-calendar'}>
+                    <div className={'asktug-sidebar-event-container-calendar-month'}>{month}</div>
+                    <div className={'asktug-sidebar-event-container-calendar-day'}>
+                      {day}
+                      {/*<CaretUpOutlined className="asktug-sidebar-event-container-calendar-body-arrow" />*/}
+                      {/*<CaretUpOutlined className="asktug-sidebar-event-container-calendar-body-arrow-oppsite" />*/}
                     </div>
                   </div>
-                </SiteLink>
-              </li>
+                  <div className={'asktug-sidebar-event-container-content'}>
+                    <h3 className={'asktug-sidebar-event-container-content-title'}>{value.title}</h3>
+                    <div className={'asktug-sidebar-event-container-content-metadata'}>
+                      {value.status && <Badge color={'#ee6d85'} text={value.status} />}
+                      {value.type && <Badge color={'#69c384'} text={value.type} />}
+                    </div>
+                  </div>
+                </div>
+              </SiteLink>
             )
           })}
-        </ul>
+        </Space>
       </SidebarCard>
     </div>
   )
