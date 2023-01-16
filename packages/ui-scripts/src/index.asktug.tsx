02c52157bac258dd42a89274241587ff557741d3
Chang Wei <changwei1006@gmail.com>
2023-01-17 03:51:26 +0800

    Revert "feature/2022-11-25-asktug-home: remove component AsktugCategories from index.asktug.tsx/AsktugSite"
    This reverts commit bb962085c72bb8dfe232644597a8b45b2d39f579.

--- packages/ui-scripts/src/index.asktug.tsx
+++ packages/ui-scripts/src/index.asktug.tsx
@@ -142,7 +142,7 @@ const AsktugSite = () => {
             <AsktugHeader/>
             <AsktugFooter/>
             <AsktugSidebar/>
-            {/*<AsktugCategories/>*/}
+            <AsktugCategories/>
         </SiteComponentsContext.Provider>
     )
 }
