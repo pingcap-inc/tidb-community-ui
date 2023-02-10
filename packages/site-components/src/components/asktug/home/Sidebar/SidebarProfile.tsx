e067d8c403ae2a15164b79b20154d4eb52a25c73
Chang Wei <changwei1006@gmail.com>
2023-02-10 17:18:24 +0800

    feature/2022-11-25-asktug-home: add icon to SidebarProfile

--- packages/site-components/src/components/asktug/home/Sidebar/SidebarProfile.tsx
+++ packages/site-components/src/components/asktug/home/Sidebar/SidebarProfile.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React from "react"
 import {Button, Col, Row, Space, Tooltip} from "antd";
 import {GiftOutlined} from "@ant-design/icons";
 
@@ -9,6 +9,11 @@ import {Site} from "../../../../utils/site";
 import {useAsktugBadges, useAsktugUserSummary} from "../../../../datasource/asktug";
 import {useBlogUsersPosts} from "../../../../datasource/blog";
 import {usePointMe} from "../../../../datasource/accounts";
+import profileLogoImage from './profile-icon.png'
+import postLogoImage from './post-icon.png'
+import likeLogoImage from './like-icon.png'
+import articleLogoImage from './article-icon.png'
+import expLogoImage from './exp-icon.png'
 
 export interface IProps extends React.HTMLAttributes<HTMLDivElement> {
   username: string
@@ -26,31 +31,43 @@ const SidebarProfile: React.FC<IProps> = (props) => {
   console.error({errorPointMe, errorBadges, errorUserSummary, errorBlogUsersPosts})
   return (
     <div className={'asktug-sidebar-profile'}>
-      <SidebarCard header={{start: '我的社区旅程'}}>
+      <SidebarCard header={{start: '我的社区旅程', end: <div className={'asktug-sidebar-profile-header'}><img src={profileLogoImage} alt="" className={'asktug-sidebar-profile-header-icon'}/> <span>上周排名</span> <span className={"asktug-sidebar-profile-header-rank-number"}>137</span></div>}}>
 
         <Row className={'asktug-sidebar-profile-count'} gutter={16}>
           <Col span={6}>
             <div className={'asktug-sidebar-profile-count-item'}>
-              <div className={'asktug-sidebar-profile-count-item-value'}>{dataUserSummary?.user_summary.post_count ?? 'N/A'}</div>
-              <div className={'asktug-sidebar-profile-count-item-key'}>帖子</div>
+              <div className={'asktug-sidebar-profile-count-item-value'}>{dataUserSummary?.user_summary.post_count ?? '-'}</div>
+              <div className={'asktug-sidebar-profile-count-item-key'}>
+                <img src={postLogoImage} alt=""/>
+                <span>帖子</span>
+              </div>
             </div>
           </Col>
           <Col span={6}>
             <div className={'asktug-sidebar-profile-count-item'}>
-              <div className={'asktug-sidebar-profile-count-item-value'}>{dataUserSummary?.user_summary.likes_received ?? 'N/A'}</div>
-              <div className={'asktug-sidebar-profile-count-item-key'}>获赞</div>
+              <div className={'asktug-sidebar-profile-count-item-value'}>{dataUserSummary?.user_summary.likes_received ?? '-'}</div>
+              <div className={'asktug-sidebar-profile-count-item-key'}>
+                <img src={likeLogoImage} alt=""/>
+                <span>获赞</span>
+              </div>
             </div>
           </Col>
           <Col span={6}>
             <div className={'asktug-sidebar-profile-count-item'}>
-              <div className={'asktug-sidebar-profile-count-item-value'}>{dataBlogUsersPosts?.page.totalElements ?? 'N/A'}</div>
-              <div className={'asktug-sidebar-profile-count-item-key'}>文章</div>
+              <div className={'asktug-sidebar-profile-count-item-value'}>{dataBlogUsersPosts?.page.totalElements ?? '-'}</div>
+              <div className={'asktug-sidebar-profile-count-item-key'}>
+                <img src={articleLogoImage} alt=""/>
+                <span>文章</span>
+              </div>
             </div>
           </Col>
           <Col span={6}>
             <div className={'asktug-sidebar-profile-count-item'}>
-              <div className={'asktug-sidebar-profile-count-item-value'}>{dataPointMe?.data.current_exps ?? 'N/A'}</div>
-              <div className={'asktug-sidebar-profile-count-item-key'}>经验值</div>
+              <div className={'asktug-sidebar-profile-count-item-value'}>{dataPointMe?.data.current_exps ?? '-'}</div>
+              <div className={'asktug-sidebar-profile-count-item-key'}>
+                <img src={expLogoImage} alt=""/>
+                <span>经验值</span>
+              </div>
             </div>
           </Col>
         </Row>
