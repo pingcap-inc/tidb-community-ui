78c5634016748efd1c276b2a2d1f5abaa39f61f2
Chang Wei <changwei1006@gmail.com>
2023-12-08 14:38:49 +0800

    update ICP data

--- packages/storybook/stories/2-site-components/footer/index.stories.jsx
+++ packages/storybook/stories/2-site-components/footer/index.stories.jsx
@@ -12,7 +12,7 @@ export const Preview = Template.bind({})
 
 Preview.args = {
   copyright: '©2021 TiDB Community.',
-  icp: '京ICP备16046278号-4',
+  icp: '京ICP备20022552号-6',
   icpUrl: 'https://beian.miit.gov.cn/',
   number: '京公网安备11010802035239号',
   numberUrl: 'http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802035239'
--- packages/ui-scripts/src/footer.data.ts
+++ packages/ui-scripts/src/footer.data.ts
@@ -2,7 +2,7 @@ import { FooterProps } from '@pingcap-inc/tidb-community-site-components'
 
 export const footerProps: FooterProps = {
   copyright: '©2021 TiDB Community.',
-  icp: '京ICP备16046278号-4',
+  icp: '京ICP备20022552号-6',
   icpUrl: 'https://beian.miit.gov.cn/',
   number: '京公网安备11010802035239号',
   numberUrl: 'http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802035239'
